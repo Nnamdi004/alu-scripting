cd644b92bf738308ad81080f7594d362f9f04815
Nnamdi004 <c.onugha@alustudent.com>
2025-07-20 15:19:33 +0200

    API

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,12 @@ import requests
 
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(
+        reddit_url, headers=headers, params={'limit': 10}, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()['data']
-        for post in data['children'][:10]:
+        for post in data['children']:
             print(post['data']['title'])
-    else:
-        print(None)
